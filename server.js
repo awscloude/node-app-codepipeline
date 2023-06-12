f231388f200bf3ea5cf4c69858c381d0a3b29953
awscloude <97865454+awscloude@users.noreply.github.com>
2023-06-13 00:15:24 +0600

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   res.send(
-    `<body style='background-color:#3393ff'><h1 style='color: orange;text-align:center'>Hello AWS ${os.hostname()}</h1></body>`
+    `<body style='background-color:#ff33a8'><h1 style='color: orange;text-align:center'>Hello AWS ${os.hostname()}</h1></body>`
   );
 });
 
